07fa3ed506b2371212c525823654e43815ebf055
Junian <git@junian.net>
2018-01-29 06:48:37 +0700

    add tcp client engine

---
+++ src/Samariterm.Core.NetFx/Engines/Networks/TcpClientEngine.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.Core.Engines.Networks
+{
+    public class TcpClientEngine: BaseNetworkEngine
+    {
+        TcpClient _tcpClient;
+
+        public TcpClientEngine()
+            : base()
+        {
+            _tcpClient = new TcpClient();
+        }
+
+        private Stream _baseStream;
+        public override Stream BaseStream
+        {
+            get => _baseStream;
+            protected set => _baseStream = value;
+        }
+
+        public override int ReadBufferSize
+        {
+            get => _tcpClient?.ReceiveBufferSize ?? 4096;
+            set
+            {
+                if (_tcpClient != null)
+                {
+                    _tcpClient.ReceiveBufferSize = value;
+                }
+            }
+        }
+
+        public override bool IsOpen
+        {
+            get
+            {
+                try
+                {
+                    if (_tcpClient != null && _tcpClient.Client != null && _tcpClient.Client.Connected)
+                    {
+                        /* pear to the documentation on Poll:
+                         * When passing SelectMode.SelectRead as a parameter to the Poll method it will return 
+                         * -either- true if Socket.Listen(Int32) has been called and a connection is pending;
+                         * -or- true if data is available for reading; 
+                         * -or- true if the connection has been closed, reset, or terminated; 
+                         * otherwise, returns false
+                         */
+
+                        // Detect if client disconnected
+                        if (_tcpClient.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            byte[] buff = new byte[1];
+                            if (_tcpClient.Client.Receive(buff, SocketFlags.Peek) == 0)
+                            {
+                                // Client disconnected
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            protected set { return; }
+        }
+
+        public override void Write(byte[] data, int offset, int count)
+        {
+            BaseStream?.Write(data, offset, count);
+        }
+
+        protected override void EngineClose()
+        {
+            BaseStream?.Dispose();
+            _tcpClient?.Close();
+            BaseStream = null;
+        }
+
+        protected override async Task EngineCloseAsync()
+        {
+            await Task.Run(() => EngineClose());
+        }
+
+        protected override void EngineOpen()
+        {
+            _tcpClient = new TcpClient();
+            _tcpClient?.Connect(IPAddress.Parse(CurrentSettings.IpAddress), CurrentSettings.Port);
+            BaseStream = _tcpClient?.GetStream();
+        }
+
+        protected override async Task EngineOpenAsync()
+        {
+            _tcpClient = new TcpClient();
+            await _tcpClient?.ConnectAsync(IPAddress.Parse(CurrentSettings.IpAddress), CurrentSettings.Port);
+            BaseStream = _tcpClient?.GetStream();
+        }
+
+        private Settings CurrentSettings;
+
+        public override void Set(object model)
+        {
+            if (model is Settings s)
+            {
+                CurrentSettings = s;
+            }
+        }
+
+        public class Settings
+        {
+            public string IpAddress { get; set; }
+            public int Port { get; set; }
+        }
+    }
+}
