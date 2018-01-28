65da84bd8e1d5a1a30af3e5021eea3ba7ab19995
Junian <git@junian.net>
2018-01-29 06:30:31 +0700

    add network engine interface

---
+++ src/Samariterm.Core/Engines/Networks/INetworkEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Juniansoft.Samariterm.Core.Events;
+
+namespace Juniansoft.Samariterm.Core.Engines.Networks
+{
+    public interface INetworkEngineIEngine
+    {
+        event EventHandler<MessageResponseReceivedArgs> MessageResponseReceived;
+
+        Stream BaseStream { get; }
+
+        int ReadBufferSize { get; set; }
+
+        void Set(object model);
+
+        void Write(byte[] data);
+        void Write(byte[] data, int offset, int count);
+        void Open();
+        Task OpenAsync();
+        void Close();
+        Task CloseAsync();
+        bool IsOpen { get; }
+    }
+}
