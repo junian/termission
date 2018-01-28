ed58405f452eab4d84dc1c9680112276d99a3797
Junian <git@junian.net>
2018-01-29 06:28:12 +0700

    add messageresponse eventargs

---
+++ src/Samariterm.Core/Events/MessageResponseReceivedArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Juniansoft.Samariterm.Core.Events
+{
+    public class MessageResponseReceivedArgs: EventArgs
+    {
+        public byte[] Data { get; private set; }
+
+        public MessageResponseReceivedArgs(byte[] data)
+        {
+            Data = data;
+        }
+    }
+}
