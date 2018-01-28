0aa1135a4eb8a48f16e6c6de65600b1828c09d22
Junian <git@junian.net>
2018-01-29 06:50:11 +0700

    add tcp listener engine

---
+++ src/Samariterm.Core.NetFx/Engines/Networks/TcpListenerEngine.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.Core.Engines.Networks
+{
+    public class TcpListenerEngine: BaseNetworkEngine
+    {
+        private TcpListener _tcpListener;
+        private TcpClient _tcpClient;
+
+        public TcpListenerEngine()
+            : base()
+        {
+            _tcpListener = default(TcpListener);
+            _tcpClient = default(TcpClient);
+        }
+
+        private Stream _baseStream;
+        public override Stream BaseStream
+        {
+            get => _baseStream;
+            protected set => _baseStream = value;
+        }
+
+        public override int ReadBufferSize
+        {
+            get => _tcpClient?.ReceiveBufferSize ?? 4096;
+            set
+            {
+                if (_tcpClient != null)
+                    _tcpClient.ReceiveBufferSize = value;
+            }
+        }
+
+        public override bool IsOpen
+        {
+            get; protected set;
+        }
+
+        public override void Write(byte[] data, int offset, int count)
+        {
+            BaseStream?.Write(data, offset, count);
+        }
+
+        protected override void EngineClose()
+        {
+            IsOpen = false;
+            BaseStream?.Dispose();
+            BaseStream = null;
+            _tcpClient?.Close();
+            _tcpListener?.Stop();
+        }
+
+        protected override async Task EngineCloseAsync()
+        {
+            await Task.Run(() => EngineClose());
+        }
+
+        protected override void EngineOpen()
+        {
+            try
+            {
+                _tcpListener = new TcpListener(IPAddress.Any, CurrentSettings.Port);
+                _tcpListener?.Start();
+                IsOpen = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                IsOpen = false;
+            }
+        }
+
+        public override void Open()
+        {
+            EngineOpen();
+            if (IsOpen)
+            {
+                _operation = AsyncOperationManager.CreateOperation(null);
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        while (IsOpen)
+                        {
+                            _tcpClient = await _tcpListener.AcceptTcpClientAsync();
+                            Console.WriteLine($"Connected with {_tcpClient}");
+                            BaseStream = _tcpClient.GetStream();
+                            while (await StreamReadAsync()) ;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                });
+            }
+        }
+
+        protected override Task EngineOpenAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Settings CurrentSettings;
+
+        public override void Set(object model)
+        {
+            if (model is Settings s)
+            {
+                CurrentSettings = s;
+            }
+        }
+
+        public class Settings
+        {
+            public string IpAddress { get; set; }
+            public int Port { get; set; }
+        }
+    }
+}
