3316afa2c9b5cef0303ace32959368e71d4d881c
Junian <git@junian.net>
2018-01-29 06:55:59 +0700

    add ibotscriptengine

---
+++ src/Samariterm.Core/Engines/Scripts/IBotScriptEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juniansoft.Samariterm.Core.Engines.Scripts
+{
+    public interface IBotScriptEngine
+    {
+        bool Compile(string code);
+        IList<string> Errors { get; }
+        byte[] GetResponse(byte[] data);
+    }
+
+    // Bot structure
+
+    public interface IProgram
+    {
+        byte[] Main(byte[] args);
+    }
+
+    public class Program : IProgram
+    {
+        public byte[] Main(byte[] args) => throw new NotImplementedException();
+    }
+
+    public class BotFactory
+    {
+        public IProgram CreateProgram() => new Program();
+    }
+}
