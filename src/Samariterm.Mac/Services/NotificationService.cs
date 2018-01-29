dc23765ce44e3ee58e52d5718bbabc394ee530af
Junian <git@junian.net>
2018-01-29 11:07:34 +0700

    add mac notification service

---
+++ src/Samariterm.Mac/Services/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Foundation;
+using Juniansoft.Samariterm.Core.Services;
+
+namespace Juniansoft.Samariterm.Mac.Services
+{
+    public class NotificationService: INotificationService
+    {
+        public NotificationService()
+        {
+        }
+
+        public void Show(string title, string message)
+        {
+            // Trigger a local notification after the time has elapsed
+            var notification = new NSUserNotification
+            {
+                // Add text and sound to the notification
+                Title = title,
+                InformativeText = message,
+                SoundName = NSUserNotification.NSUserNotificationDefaultSoundName,
+                HasActionButton = false
+            };
+            NSUserNotificationCenter.DefaultUserNotificationCenter.DeliverNotification(notification);
+        }
+    }
+}
