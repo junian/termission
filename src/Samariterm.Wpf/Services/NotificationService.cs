5409e42ec3f680c18f33480fb33e2190b446c645
Junian <git@junian.net>
2018-01-29 12:49:32 +0700

    add notification service

---
+++ src/Samariterm.Wpf/Services/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿using Juniansoft.Samariterm.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.Wpf.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public void Show(string title, string message)
+        {
+
+        }
+    }
+}
