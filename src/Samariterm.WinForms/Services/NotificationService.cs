6d3aded0478fa11611986aac4fa7a69dfd9ebe82
Junian <git@junian.net>
2018-01-29 12:40:19 +0700

    add notification service

---
+++ src/Samariterm.WinForms/Services/NotificationService.cs
@@ -0,0 +1,33 @@
+﻿using Juniansoft.Samariterm.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juniansoft.Samariterm.WinForms.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private NotifyIcon _notifyIcon;
+
+        public NotificationService()
+        {
+            _notifyIcon = new NotifyIcon()
+            {
+                BalloonTipTitle = "SDEmu",
+                //Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon"))),
+                Text = "SDEmu",
+                Visible = true,
+            };
+        }
+
+        public void Show(string title, string message)
+        {
+            _notifyIcon.BalloonTipTitle = title;
+            _notifyIcon.BalloonTipText = message;
+            _notifyIcon.ShowBalloonTip(250);
+        }
+    }
+}
