a747c1d39393359adb99771c9157a4ee68b756e5
Junian <git@junian.net>
2018-01-29 09:42:56 +0700

    add crossdialog service

---
+++ src/Samariterm.EtoForms/Services/CrossDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Eto.Forms;
+using Juniansoft.Samariterm.Core.Services;
+
+namespace Juniansoft.Samariterm.EtoForms.Services
+{
+    public class CrossDialog: ICrossDialog
+    {
+        public Task<string> ShowOpenDialogAsync(string extension = "json", string typename = "JSON Files")
+        {
+            return Task.FromResult<string>(ShowOpenDialog(extension, typename));
+        }
+
+        public string ShowOpenDialog(string extension = "json", string typename = "JSON Files")
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                MultiSelect = false,
+                Filters =
+                {
+                    new FileDialogFilter(typename, $".{extension}"),
+                },
+            };
+            if (openFileDialog.ShowDialog(null) == DialogResult.Ok)
+            {
+                return openFileDialog.FileName;
+            }
+            return string.Empty;
+        }
+
+        public string ShowSaveDialog(string extension = "json", string typename = "JSON Files")
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filters =
+                {
+                    new FileDialogFilter(typename, $".{extension}"),
+                },
+            };
+            if (saveFileDialog.ShowDialog(null) == DialogResult.Ok)
+            {
+                return saveFileDialog.FileName;
+            }
+            return string.Empty;
+        }
+
+        public Task<string> ShowSaveDialogAsync(string extension = "json", string typename = "JSON Files")
+        {
+            return Task.FromResult<string>(ShowSaveDialog(extension, typename));
+        }
+
+        public async Task ShowMessageBoxAsync(string message)
+        {
+            await Task.Run(() => MessageBox.Show(message));
+        }
+    }
+}
