ea18f900411bdf66e160e2229fdd947a832c77ba
Junian <git@junian.net>
2018-01-29 10:07:03 +0700

    add coreviewmodel

---
+++ src/Samariterm.Core/ViewModels/CoreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Juniansoft.Samariterm.Core.Events;
+
+namespace Juniansoft.Samariterm.Core.ViewModels
+{
+    public class CoreViewModel: BaseViewModel
+    {
+        public event EventHandler<LogReceivedEventArgs> LogReceived;
+
+        protected void OnLogReceived(object message, string logLevel = "INFO")
+        {
+            LogReceived?.Invoke(this, new LogReceivedEventArgs(message?.ToString(), logLevel));
+        }
+    }
+}
