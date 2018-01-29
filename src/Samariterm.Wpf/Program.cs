f36ade679db94924786579ef2f2962c680d0171c
Junian <git@junian.net>
2018-01-29 13:04:02 +0700

    can run on wpf

---
+++ src/Samariterm.Wpf/Program.cs
@@ -0,0 +1,34 @@
+﻿using Juniansoft.Samariterm.Core;
+using Juniansoft.Samariterm.Core.Services;
+using Juniansoft.Samariterm.EtoForms;
+using Juniansoft.Samariterm.EtoForms.Controls;
+using Juniansoft.Samariterm.EtoForms.Forms;
+using Juniansoft.Samariterm.Wpf.Controls;
+using Juniansoft.Samariterm.Wpf.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.Wpf
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var platform = Eto.Platform.Get(Eto.Platforms.Wpf);
+
+            platform.Add<SyntaxHightlightTextArea.ISyntaxHightlightTextArea>(() => new SyntaxHightlightTextAreaHandler());
+
+            //ConfigureStyles();
+
+            var app = new MainApplication(platform);
+
+            ServiceLocator.Instance.Register<INotificationService, NotificationService>();
+
+            app.Run(new MainForm());
+        }
+    }
+}
