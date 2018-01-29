e36cc0d38391eb315423599d5130e892c9325618
Junian <git@junian.net>
2018-01-29 09:28:12 +0700

    add string extension

---
+++ src/Samariterm.Core/Extensions/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Juniansoft.Samariterm.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Truncate(this string value, int maxLength, string ending = "")
+        {
+            if (string.IsNullOrEmpty(value)) { return value; }
+
+            if (value.Length > maxLength) { return $"{value.Substring(0, maxLength)}{ending}"; }
+
+            return value;
+        }
+    }
+}
