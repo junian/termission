e13a6141cc0e1eea4e4ae789c21e11cb38d344f1
Junian <git@junian.net>
2018-01-29 09:37:37 +0700

    add botscript model and settings

--- src/Samariterm.Core/Helpers/Settings.cs
+++ src/Samariterm.Core/Helpers/Settings.cs
@@ -1,43 +1,192 @@
 // Helpers/Settings.cs
+using Juniansoft.Samariterm.Core.Enums;
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
 namespace Juniansoft.Samariterm.Core.Helpers
 {
-	/// <summary>
-	/// This is the Settings static class that can be used in your Core solution or in any
-	/// of your client applications. All settings are laid out the same exact way with getters
-	/// and setters. 
-	/// </summary>
-	public static class Settings
-	{
-		private static ISettings AppSettings
-		{
-			get
-			{
-				return CrossSettings.Current;
-			}
-		}
+    /// <summary>
+    /// This is the Settings static class that can be used in your Core solution or in any
+    /// of your client applications. All settings are laid out the same exact way with getters
+    /// and setters. 
+    /// </summary>
+    public static class Settings
+    {
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
 
-		#region Setting Constants
+        public const string DefaultTcpListenerIpAddress = "127.0.0.1";
+        public static string TcpListenerIpAddress
+        {
+            get => AppSettings.GetValueOrDefault(nameof(TcpListenerIpAddress), DefaultTcpListenerIpAddress);
+            set => AppSettings.AddOrUpdateValue(nameof(TcpListenerIpAddress), value);
+        }
 
-		private const string SettingsKey = "settings_key";
-		private static readonly string SettingsDefault = string.Empty;
+        public const int DefaultTcpListenerPort = 11500;
+        public static int TcpListenerPort
+        {
+            get => AppSettings.GetValueOrDefault(nameof(TcpListenerPort), DefaultTcpListenerPort);
+            set => AppSettings.AddOrUpdateValue(nameof(TcpListenerPort), value);
+        }
 
-		#endregion
+        public const string DefaultTcpClientIpAddress = "127.0.0.1";
+        public static string TcpClientIpAddress
+        {
+            get => AppSettings.GetValueOrDefault(nameof(TcpClientIpAddress), DefaultTcpClientIpAddress);
+            set => AppSettings.AddOrUpdateValue(nameof(TcpClientIpAddress), value);
+        }
 
+        public const int DefaultTcpClientPort = 11500;
+        public static int TcpClientPort
+        {
+            get => AppSettings.GetValueOrDefault(nameof(TcpClientPort), DefaultTcpClientPort);
+            set => AppSettings.AddOrUpdateValue(nameof(TcpClientPort), value);
+        }
 
-		public static string GeneralSettings
-		{
-			get
-			{
-				return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
-			}
-			set
-			{
-				AppSettings.AddOrUpdateValue(SettingsKey, value);
-			}
-		}
+        public static bool DeviceBotEnabled
+        {
+            get => AppSettings.GetValueOrDefault(nameof(DeviceBotEnabled), false);
+            set => AppSettings.AddOrUpdateValue(nameof(DeviceBotEnabled), value);
+        }
 
-	}
+        public static bool IsCR
+        {
+            get => AppSettings.GetValueOrDefault(nameof(IsCR), false);
+            set => AppSettings.AddOrUpdateValue(nameof(IsCR), value);
+        }
+
+        public static bool IsLF
+        {
+            get => AppSettings.GetValueOrDefault(nameof(IsLF), false);
+            set => AppSettings.AddOrUpdateValue(nameof(IsLF), value);
+        }
+
+        public static string SendMessage
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SendMessage), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(SendMessage), value);
+        }
+
+        public static bool MainFormTopMost
+        {
+            get => AppSettings.GetValueOrDefault(nameof(MainFormTopMost), false);
+            set => AppSettings.AddOrUpdateValue(nameof(MainFormTopMost), value);
+        }
+
+        public static int SelectedActivityMode
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedActivityMode), (int)ActivityMode.String);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedActivityMode), value);
+        }
+
+        public static int SelectedBaudRate
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedBaudRate), 19200);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedBaudRate), value);
+        }
+
+        public static int SelectedDataBits
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedDataBits), 8);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedDataBits), value);
+        }
+
+        public static int SelectedStopBits
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedStopBits), 0);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedStopBits), value);
+        }
+
+        public static int SelectedHandshake
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedHandshake), 0);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedHandshake), value);
+        }
+
+        public static int SelectedParity
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedParity), 0);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedParity), value);
+        }
+
+        public static int SelectedScriptingLanguage
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedScriptingLanguage), (int)BotScriptType.CSharp);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedScriptingLanguage), value);
+        }
+
+        public const string DefaultCSharpBotScript =
+@"// Return value must be array of bytes
+// Return null if it means no response.
+// Use `byte[] args` to see received message.
+// Use Encoding.ASCII.GetBytes(string) to return a string response.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Do not remove this `Program` class and its `Main` function. 
+// It is mandatory for your script to work properly.
+// Other than that, feel free to create any classes and functions.
+public class Program: IProgram
+{
+    public byte[] Main(byte[] args)
+    {
+        // Put your response code here
+        
+        return null;
+    }
+}
+";
+
+        public const string DefaultJSBotScript =
+@"
+function main(args)
+{
+    return null;
+}
+";
+
+        public const string DefaultPythonBotScript =
+@"
+
+";
+
+        public static string CurrentBotScript
+        {
+            get => AppSettings.GetValueOrDefault(nameof(CurrentBotScript), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(CurrentBotScript), value);
+        }
+
+        public static string CurrentSettingsFile
+        {
+            get => AppSettings.GetValueOrDefault(nameof(CurrentSettingsFile), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(CurrentSettingsFile), value);
+        }
+
+        public static string CurrentBotFile
+        {
+            get => AppSettings.GetValueOrDefault(nameof(CurrentBotFile), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(CurrentBotFile), value);
+        }
+
+        public static int DeviceBotWidth
+        {
+            get => AppSettings.GetValueOrDefault(nameof(DeviceBotWidth), 640);
+            set => AppSettings.AddOrUpdateValue(nameof(DeviceBotWidth), value);
+        }
+
+        public static int DeviceBotHeight
+        {
+            get => AppSettings.GetValueOrDefault(nameof(DeviceBotHeight), 480);
+            set => AppSettings.AddOrUpdateValue(nameof(DeviceBotHeight), value);
+        }
+    }
 }
--- src/Samariterm.Core/Models/BotScript.cs
+++ src/Samariterm.Core/Models/BotScript.cs
@@ -1,10 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Juniansoft.Samariterm.Core.Enums;
+using Juniansoft.Samariterm.Core.Helpers;
+
 namespace Juniansoft.Samariterm.Core.Models
 {
     public class BotScript
     {
-        public BotScript()
+        public const string CSharpFileExtension = "csbot";
+        public const string CSharpFileName = "C# Bot Script";
+        public const string JavaScriptFileExtension = "jsbot";
+        public const string JavaScriptFileName = "JS Bot Script";
+        public const string PythonFileExtension = "pybot";
+        public const string PythonFileName = "Python Bot Script";
+
+        public BotScriptType ScriptType { get; private set; }
+        public string FileExtension { get; private set; }
+        public string FileTypeName { get; private set; }
+        public string DefaultScript { get; private set; }
+
+        private BotScript() { }
+
+        private static IDictionary<BotScriptType, BotScript> _all = null;
+        public static IDictionary<BotScriptType, BotScript> All
         {
+            get
+            {
+                if (_all == null)
+                {
+                    _all = new List<BotScript>
+                    {
+                        new BotScript
+                        {
+                            ScriptType = BotScriptType.CSharp,
+                            FileExtension = CSharpFileExtension,
+                            FileTypeName = CSharpFileName,
+                            DefaultScript = Settings.DefaultCSharpBotScript,
+                        },
+                        new BotScript
+                        {
+                            ScriptType = BotScriptType.JavaScript,
+                            FileExtension = JavaScriptFileExtension,
+                            FileTypeName = JavaScriptFileName,
+                            DefaultScript = Settings.DefaultJSBotScript,
+                        },
+                        //new BotScript
+                        //{
+                        //    ScriptType = BotScriptType.Python,
+                        //    FileExtension = PythonFileExtension,
+                        //    FileTypeName = PythonFileName,
+                        //    DefaultScript = Settings.DefaultPythonBotScript,
+                        //},
+                    }.ToDictionary(x => x.ScriptType);
+                }
+                return _all;
+            }
         }
+
     }
 }
