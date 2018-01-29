4839cb424a1855727470bc4220fe694221f71180
Junian <git@junian.net>
2018-01-29 09:08:14 +0700

    add Command, ServiceLocator, and BaseViewModel

---
+++ src/Samariterm.Core/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juniansoft.Samariterm.Core
+{
+    public sealed class ServiceLocator
+    {
+        static readonly Lazy<ServiceLocator> instance = new Lazy<ServiceLocator>(() => new ServiceLocator());
+        readonly Dictionary<Type, Lazy<object>> registeredServices = new Dictionary<Type, Lazy<object>>();
+
+        public static ServiceLocator Instance => instance.Value;
+
+        public void Register<TContract, TService>() where TService : new()
+        {
+            registeredServices[typeof(TContract)] =
+                new Lazy<object>(() => Activator.CreateInstance(typeof(TService)));
+        }
+
+        public T Get<T>() where T : class
+        {
+            Lazy<object> service;
+            if (registeredServices.TryGetValue(typeof(T), out service))
+            {
+                return (T)service.Value;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Samariterm.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Juniansoft.Samariterm.Core.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
