a17a61f3b4e9bd9b5b3c39eed3377a326cddbd25
Junian <git@junian.net>
2018-01-29 11:01:35 +0700

    add preferences form

---
+++ src/Samariterm.EtoForms/Forms/PreferencesForm.cs
@@ -0,0 +1,24 @@
+﻿using Eto.Forms;
+using Juniansoft.Samariterm.Core.ViewModels;
+using Juniansoft.Samariterm.EtoForms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.EtoForms.Forms
+{
+    public class PreferencesForm : Dialog
+    {
+        public PreferencesForm()
+        {
+            var vm = Core.ServiceLocator.Instance.Get<PreferencesViewModel>();
+            var view = Core.ServiceLocator.Instance.Get<PreferencesView>();
+
+            this.Content = view;
+            this.Title = "Preferences";
+            this.DataContext = vm;
+        }
+    }
+}
