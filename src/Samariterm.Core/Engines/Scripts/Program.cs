253e812a52f35a415c501318bc35d66b09e4237f
Junian <git@junian.net>
2018-01-29 12:17:19 +0700

    fix the order of cs template

--- src/Samariterm.Core/Engines/Scripts/Program.cs
+++ src/Samariterm.Core/Engines/Scripts/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace Juniansoft.Samariterm.Core.Engines.Scripts
 {
+    public class Program : IProgram { public byte[] Main(byte[] args) => throw new NotImplementedException(); }
+
     public interface IProgram
     {
         byte[] Main(byte[] args);
     }
-
-    public class Program : IProgram { public byte[] Main(byte[] args) => throw new NotImplementedException(); }
 
     public class BotFactory
     {
