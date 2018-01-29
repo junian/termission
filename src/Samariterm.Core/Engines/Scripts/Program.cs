44ec3d5cd6b72aea603cbdaf64a4c0fc055e1107
Junian <git@junian.net>
2018-01-29 07:37:47 +0700

    add cs bot

---
+++ src/Samariterm.Core/Engines/Scripts/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Juniansoft.Samariterm.Core.Engines.Scripts
+{
+    public interface IProgram
+    {
+        byte[] Main(byte[] args);
+    }
+
+    public class Program : IProgram
+    {
+        public byte[] Main(byte[] args) => throw new NotImplementedException();
+    }
+
+    public class BotFactory
+    {
+        public IProgram CreateProgram() => new Program();
+    }
+}
