abe480924b3b5739f09c5e80e62e1f09385f0fbf
Junian <git@junian.net>
2018-01-29 09:27:27 +0700

    add eto styles

---
+++ src/Samariterm.Core/Constants/EtoStyles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Juniansoft.Samariterm.Core.Constants
+{
+    public static class EtoStyles
+    {
+        public const string FormMain = nameof(FormMain);
+        public const string DeviceBotDialog = nameof(DeviceBotDialog);
+        public const string SourceEditor = nameof(SourceEditor);
+        public const string SendCommandText = nameof(SendCommandText);
+        public const string Toolbar = nameof(Toolbar);
+        public const string ButtonToolItem = nameof(ButtonToolItem);
+    }
+}
