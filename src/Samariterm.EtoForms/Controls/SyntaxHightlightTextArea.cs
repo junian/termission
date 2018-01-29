2c134bd547cb8fc1493f80672473f64cf85e8c38
Junian <git@junian.net>
2018-01-29 09:44:59 +0700

    add syntaxhighlight area interface control

---
+++ src/Samariterm.EtoForms/Controls/SyntaxHightlightTextArea.cs
@@ -0,0 +1,28 @@
+﻿using Eto;
+using Eto.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juniansoft.Samariterm.EtoForms.Controls
+{
+    [Handler(typeof(ISyntaxHightlightTextArea))]
+    public class SyntaxHightlightTextArea : TextArea
+    {
+        new ISyntaxHightlightTextArea Handler { get { return (ISyntaxHightlightTextArea)base.Handler; } }
+
+        public string CodeLanguage
+        {
+            get { return Handler.CodeLanguage; }
+            set { Handler.CodeLanguage = value; }
+        }
+
+        // interface to the platform implementations
+        public interface ISyntaxHightlightTextArea : IHandler
+        {
+            string CodeLanguage { get; set; }
+        }
+    }
+}
