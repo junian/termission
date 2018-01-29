11dc2ce450a5a42318cd72856aff5ee4380db071
Junian <git@junian.net>
2018-01-29 09:36:05 +0700

    add termsharpsettings

---
+++ src/Samariterm.Core/Models/TermSharpSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Juniansoft.Samariterm.Core.Enums;
+
+namespace Juniansoft.Samariterm.Core.Models
+{
+    public class TermSharpSettings
+    {
+        public TermSharpSettings()
+        {
+            SelectedBaudRate = 19200;
+            SelectedDataBits = 8;
+            SelectedHandshake = 0;
+            SelectedParity = 0;
+            SelectedStopBits = 0;
+
+            SelectedActivityMode = ActivityMode.String;
+            IsCR = IsLF = false;
+        }
+
+        public int SelectedBaudRate { get; set; }
+        public int SelectedDataBits { get; set; }
+        public int SelectedHandshake { get; set; }
+        public int SelectedParity { get; set; }
+        public int SelectedStopBits { get; set; }
+        public ActivityMode SelectedActivityMode { get; set; }
+        public bool IsLF { get; set; }
+        public bool IsCR { get; set; }
+    }
+}
