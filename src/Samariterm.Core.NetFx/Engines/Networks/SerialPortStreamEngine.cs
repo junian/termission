0204e71d7f9626644709f5fdd2cbb49256c3f216
Junian <git@junian.net>
2018-01-30 11:36:05 +0700

    add serialportstream engine

---
+++ src/Samariterm.Core.NetFx/Engines/Networks/SerialPortStreamEngine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Juniansoft.Samariterm.Core.Models;
+using RJCP.IO.Ports;
+
+namespace Juniansoft.Samariterm.Core.Engines.Networks
+{
+    public class SerialPortStreamEngine : BaseNetworkEngine
+    {
+        private SerialPortStream _serialPort;
+
+        public SerialPortStreamEngine()
+            : base()
+        {
+            _serialPort = new SerialPortStream();
+            //_serialPort.ErrorReceived += (_, e) => OnLogReceived(e.EventType);
+            //_serialPort.PinChanged += (_, e) => OnLogReceived(e.EventType);
+        }
+
+        public override Stream BaseStream
+        {
+            get => _serialPort;
+            protected set
+            {
+                return;
+            }
+        }
+
+        public override int ReadBufferSize
+        {
+            get => _serialPort?.ReadBufferSize ?? 4096;
+            set
+            {
+                if (_serialPort != null)
+                    _serialPort.ReadBufferSize = value;
+            }
+        }
+
+        public override bool IsOpen
+        {
+            get => _serialPort?.IsOpen ?? false;
+            protected set { return; }
+        }
+
+        public override void Set(object model)
+        {
+            if (model is SerialComSettings s)
+            {
+                _serialPort.BaudRate = s.BaudRate;
+                _serialPort.Handshake = (Handshake)s.Handshake;
+                _serialPort.Parity = (Parity)s.Parity;
+                _serialPort.DataBits = s.DataBits;
+                _serialPort.StopBits = (StopBits)s.StopBits;
+            }
+        }
+
+        public override void Write(byte[] data, int offset, int count)
+        {
+            _serialPort?.Write(data, offset, count);
+        }
+
+        protected override void EngineClose()
+        {
+            _serialPort?.DiscardOutBuffer();
+            _serialPort?.DiscardInBuffer();
+            _serialPort?.Close();
+        }
+
+        protected override async Task EngineCloseAsync()
+        {
+            await Task.Run(() => EngineClose());
+        }
+
+        protected override void EngineOpen()
+        {
+            _serialPort?.Open();
+        }
+
+        protected override async Task EngineOpenAsync()
+        {
+            await Task.Run(() => EngineOpen());
+        }
+    }
+
+}
