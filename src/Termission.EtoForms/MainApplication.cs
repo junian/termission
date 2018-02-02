11469eb13f5557c44e255988da6c20610086adc2
Junian <git@junian.net>
2018-02-02 08:21:33 +0700

    add mono options for help

--- src/Termission.EtoForms/MainApplication.cs
+++ src/Termission.EtoForms/MainApplication.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Eto;
 using Eto.Forms;
@@ -10,6 +11,7 @@ using Juniansoft.Termission.Core.ViewModels;
 using Juniansoft.Termission.EtoForms.Forms;
 using Juniansoft.Termission.EtoForms.Services;
 using Juniansoft.Termission.EtoForms.Views;
+using Mono.Options;
 
 namespace Juniansoft.Termission.EtoForms
 {
@@ -19,6 +21,63 @@ namespace Juniansoft.Termission.EtoForms
             : base(platform)
         {
             RegisterServices();
+        }
+
+        public void Run(string[] args)
+        {
+            // these variables will be set when the command line is parsed
+            var verbosity = 0;
+            var shouldShowHelp = false;
+
+            // these are the available options, not that they set the variables
+            var options = new OptionSet {
+                { 
+                    "v", "increase debug message verbosity", v => {
+                    if (v != null)
+                        ++verbosity;
+                    } 
+                },
+                {
+                    "h|help", "show this message and exit", h => shouldShowHelp = h != null
+                },
+                // default
+                { "<>", v =>
+                    {
+                        shouldShowHelp = true;
+                        Console.WriteLine("Unknown command parameter.");
+                    }
+                },
+            };
+
+            var extra = default(List<string>);
+            try
+            {
+                // parse the command line
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                // output some error message
+                Console.Write("termission: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `termission --help' for more information.");
+                return;
+            }
+
+            if (shouldShowHelp)
+            {
+                // show some app description message
+                Console.WriteLine("Usage: termission.exe [OPTIONS]");
+                Console.WriteLine("Cross-platform Serial/TCP terminal.");
+                Console.WriteLine();
+
+                // output the options
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            this.Run(new MainForm());
         }
 
         public void RegisterServices()
@@ -43,11 +102,6 @@ namespace Juniansoft.Termission.EtoForms
             ServiceLocator.Current.Register<MainView>();
             ServiceLocator.Current.Register<DeviceBotView>();
             ServiceLocator.Current.Register<PreferencesView>();
-        }
-
-        public void Run(string[] args)
-        {
-            this.Run(new MainForm());
         }
 
         #region Assembly Attribute Accessors
