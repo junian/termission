9aaaa1f335a1ef121e892e802b39947dc8f3029b
Junian Triajianto <author@junian.net>
2018-05-25 19:10:48 +0700

    Add basic gtk sharp project

---
+++ src/Termission.GtkSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace Juniansoft.Termission.GtkSharp
+{
+	class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Gtk).Run(new Form());
+		}
+	}
+}
