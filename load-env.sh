5067979701183c7d4bf88a03a1dba948b2e9ac4e
Junian Triajianto <author@junian.net>
2018-05-26 11:18:26 +0700

    Using .env.example to load variable

--- load-env.sh
+++ load-env.sh
@@ -2,7 +2,7 @@
 
 if [ -f ./.env ]; then
    source ./.env
-   export $(cut -d= -f1 ./.env)
+   export $(cut -d= -f1 ./.env.example)
 else
     echo "Unable to find .env file." >&2
     echo "Create a new .env by copying .env.example file." >&2
